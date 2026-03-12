89046084a73719f22c029d63084817691cd2d967
hwanzanghagetne <dltmdghks543@gmail.com>
2026-03-12 21:48:20 +0900

    feat: 세션 기반 로그인 API 추가

--- src/main/java/com/hwan/coupon/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hwan/coupon/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hwan.coupon.global.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +14,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity
+                .status(401)
+                .body(new ErrorResponse(401, "이메일 또는 비밀번호가 올바르지 않습니다"));
     }
 }
